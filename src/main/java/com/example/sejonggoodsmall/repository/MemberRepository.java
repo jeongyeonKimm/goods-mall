8d058daeb5b0e3ea5cc18cf63b57c7f40ddfff76
jeongyeonKimm <wjddus9132@gmail.com>
2023-01-27 10:45:05 +0900

    feat: 회원 리포지토리 구현

---
+++ src/main/java/com/example/sejonggoodsmall/repository/MemberRepository.java
@@ -0,0 +1,29 @@
+package com.example.sejonggoodsmall.repository;
+
+import com.example.sejonggoodsmall.model.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long memberId) {
+        return em.find(Member.class, memberId);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+    
+}
