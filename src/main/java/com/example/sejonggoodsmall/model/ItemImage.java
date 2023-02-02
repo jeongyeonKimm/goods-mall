da38c66fc668d6cabd109e1d2f056ad9d8612b7a
jeongyeonKimm <wjddus9132@gmail.com>
2023-02-02 10:14:28 +0900

    refactor: 상품 이미지 엔티티 수정

--- src/main/java/com/example/sejonggoodsmall/model/ItemImage.java
+++ src/main/java/com/example/sejonggoodsmall/model/ItemImage.java
@@ -1,10 +1,9 @@
 package com.example.sejonggoodsmall.model;
 
-import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.extern.jbosslog.JBossLog;
 
 import javax.persistence.*;
 
@@ -12,6 +11,8 @@ import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "ITEM_IMAGES")
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class ItemImage extends BaseEntity {
@@ -20,18 +21,22 @@ public class ItemImage extends BaseEntity {
     @Column(name = "ITEM_IMAGES_ID")
     private Long id;
 
+    @Column(columnDefinition = "LONGTEXT")
     private String imgName; //이미지 파일명
 
+    @Column(columnDefinition = "LONGTEXT")
     private String oriImgName; //원본 이미지 파일명
 
     @Column(columnDefinition = "LONGTEXT")
     private String imgUrl; //이미지 조회 경로
 
+    @Lob
     private String repImgUrl; //대표 이미지 여부
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ITEM_ID")
     private Item item;
+
 
     public void updateItemImage(String imgName, String oriImgName, String imgUrl) {
         this.imgName = imgName;
